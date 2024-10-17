b9f170832cb17c74e738b09c77d78bf3f02db231
Priscilla Oluwatoyin Ajayi <ajayipriscilla2013@gmail.com>
2024-10-17 02:22:37 +0100

    application form edit

--- std_app_form.php
+++ std_app_form.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tutbits</title>
-    <link rel="icon" type="image/png" href="assets/img/pic10 logo.jpg">
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
